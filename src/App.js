bafb5beb9cd09381881dab8767be2de413c30c66
lemonade-21 <sah.kum.rt22@dypatil.edu>
2025-08-19 02:41:47 +0530

    Blog Changes

--- src/App.js
+++ src/App.js
@@ -190,7 +190,7 @@ function App() {
                       <div>RÉSUMÉ</div>
                       <div>================</div>
                       <div id="z">
-                        <a href="/SahilKumar.pdf" target="_blank" rel="noopener noreferrer">
+                        <a href="/public/Sahilkumar.pdf" target="_blank" rel="noopener noreferrer">
                           resume
                         </a>
                       </div>
@@ -206,7 +206,7 @@ function App() {
                       <div id="z">
                         [
                         <span id="link">
-                          <a href="https://github.com/lemonade-21">link</a>
+                          <a href="https://lemonade-21.github.io/Blog-Page/">link</a>
                         </span>
                         ]
                       </div>
